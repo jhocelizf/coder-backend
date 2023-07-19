019a57415ae280a78119d576dbea7dad853dc115
Jhoceliz Figueroa <jhocelizf@gmail.com>
2023-07-19 17:29:30 -0300

    tarea websocket

---
+++ README.md
@@ -0,0 +1,15 @@
+# Motores de plantillas + Websocket
+
+## Start
+
+```shell
+npm install
+```
+
+## Endpoints
+
+- URL: http://localhost:8080/
+
+- GET /: Obtiene la lista estática de productos agregados hasta el momento.
+
+- GET /realtimeproducts: Obtiene la lista dinámica de productos con la implementación de Websocket. 
--- src/app.js
+++ src/app.js
@@ -1,24 +1,71 @@
 import express from "express";
 import productRouter from "./router/products.router.js";
 import cartRouter from "./router/carts.router.js";
+import viewsRouter from "./router/views.router.js";
+import realtimeRouter from "./router/realTimeProduct.router.js";
+import { Server } from "socket.io";
+import { engine } from "express-handlebars";
+import { __filename, __dirname } from "./utils.js";
+import { guardarProducto } from "./services/productUtils.js";
 
 const app = express();
 const PORT = 8080;
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
-app.get("/", (req, res) => {
-    res.send("hola mundo");
-});
+app.engine("handlebars", engine());
+app.set("view engine", "handlebars");
+app.set("views", "./src/views");
 
 app.use("/api/products", productRouter);
-
 app.use("/api/carts", cartRouter);
+app.use("/", viewsRouter);
+app.use("/realtime", realtimeRouter);
 
-app.listen(PORT, () => {
+const httpServer = app.listen(PORT, () => {
     console.log(`El servidor esta corriendo en el puerto ${PORT}`);
 });
+
+httpServer.on("error", (err) => {
+    console.log(err);
+});
+
+const io = new Server(httpServer);
+
+io.on("connection", (socket) => {
+    console.log("Nuevo cliente conectado");
+
+    // Manejar eventos personalizados
+    socket.on("mensaje", (data) => {
+        console.log("Mensaje recibido:", data);
+
+        // Enviar una respuesta al cliente
+        socket.emit("respuesta", "Mensaje recibido correctamente");
+    });
+
+    // Escuchar evento 'agregarProducto' y emitir 'nuevoProductoAgregado'
+    socket.on("agregarProducto", (newProduct) => {
+        console.log("Nuevo producto recibido backend:", newProduct);
+        guardarProducto(newProduct);
+        // Agregar el nuevo producto a la lista de productos
+        io.emit("nuevoProductoAgregado", newProduct);
+    });
+
+    socket.on("productoEliminado", (productID) => {
+      // Eliminar el producto de la lista en el cliente
+    const productoElement = document.querySelector(`[data-id="${productID}"]`);
+    if (productoElement) {
+        productoElement.parentElement.remove();
+    }
+    });
+
+    socket.on("disconnect", () => {
+        console.log("Cliente desconectado");
+    });
+});
+
 
 
 
