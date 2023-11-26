d0eae9b770184ecfe7221338d16983cad775b20e
Jhoceliz Figueroa <jhocelizf@gmail.com>
2023-11-26 16:43:05 -0300

    pf change

--- src/router/payment.router.js
+++ src/router/payment.router.js
@@ -1,9 +1,8 @@
 import { Router } from "express";
-import { createOrder, captureOrder, cancelOrder } from "../controller/payment.controller.js";
+import { captureOrder, cancelOrder } from "../controller/payment.controller.js";
 
 const paymentsRouter = Router()
 
-paymentsRouter.post("/create-order", createOrder);
 paymentsRouter.get("/capture-order",captureOrder)
 paymentsRouter.get("/cancel-order",cancelOrder)
 
